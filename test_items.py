f744088dce8c6bf071b279146169b0c9814587a8
vladf <vladforwork@yandex.ru>
2021-01-24 15:51:02 +0300

    - fixed assert message

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-pytest --language=es
 
 LINK = 'http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/'
 
@@ -6,4 +6,4 @@ LINK = 'http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/'
 def test_items(browser, language):
     browser.get(LINK.format(language))
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(),\
-        'Кнопка подобаления товара в корзину не отображается на стрнице'
+        'Кнопка добаления товара в корзину не отображается на стрнице'
