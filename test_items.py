e3738646e241c71852ce5ad06686cc0156cd08bb
vladf <vladforwork@yandex.ru>
2021-01-24 15:55:15 +0300

    - fixed method name

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@
 LINK = 'http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/'
 
 
-def test_items(browser, language):
+def test_add_to_basket_button(browser, language):
     browser.get(LINK.format(language))
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(),\
         'Кнопка добаления товара в корзину не отображается на стрнице'
