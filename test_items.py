e6aa33e7a5b06c02635df25b1060e465ee869975
vladf <vladforwork@yandex.ru>
2021-01-24 15:45:43 +0300

    - added README.md - added link formation with string format

--- test_items.py
+++ test_items.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+LINK = 'http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/'
+
 
 def test_items(browser, language):
-    browser.get(f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/')
-
+    browser.get(LINK.format(language))
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(),\
         'Кнопка подобаления товара в корзину не отображается на стрнице'
